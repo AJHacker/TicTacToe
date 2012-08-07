6ab77698b743c0d1db7afdfdf4a6e5719bd411f5
Arihant Jain <Arihant@Arihants-MacBook-Pro.local>
2012-08-07 16:22:57 -0400

    Initial Commit

---
+++ TICTACTOETests/TICTACTOETests.h
@@ -0,0 +1,13 @@
+//
+//  TICTACTOETests.h
+//  TICTACTOETests
+//
+//  Created by Arihant Jain on 8/7/12.
+//  Copyright (c) 2012 DMA. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface TICTACTOETests : SenTestCase
+
+@end
---
+++ TICTACTOETests/TICTACTOETests.m
@@ -0,0 +1,32 @@
+//
+//  TICTACTOETests.m
+//  TICTACTOETests
+//
+//  Created by Arihant Jain on 8/7/12.
+//  Copyright (c) 2012 DMA. All rights reserved.
+//
+
+#import "TICTACTOETests.h"
+
+@implementation TICTACTOETests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in TICTACTOETests");
+}
+
+@end
